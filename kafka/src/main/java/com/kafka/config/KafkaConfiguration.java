186b230c58b0a262ff6d75c8e966b7609286db49
Souvik1234 <souvikadhikary45@gmail.com>
2019-02-01 16:00:03 +0530

    working copy==read stackoverflow

--- kafka/src/main/java/com/kafka/config/KafkaConfiguration.java
+++ kafka/src/main/java/com/kafka/config/KafkaConfiguration.java
@@ -1,10 +1,12 @@
 package com.kafka.config;
 
 import com.kafka.Student;
+
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
@@ -30,7 +32,8 @@ public class KafkaConfiguration {
         return new DefaultKafkaProducerFactory(config);
     }*/
     @Bean
-    public ConsumerFactory<String, String> consumerFactory(){
+   
+    public ConsumerFactory<Object, Object> consumerFactory(){
         Map<String,Object> config = new HashMap<>();
         config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
         config.put(ConsumerConfig.GROUP_ID_CONFIG, "test-group");
@@ -41,8 +44,9 @@ public class KafkaConfiguration {
     }
 
     @Bean
-    public ConcurrentKafkaListenerContainerFactory<String,String> concurrentKafkaListenerContainerFactory(){
-        ConcurrentKafkaListenerContainerFactory<String,String> factory =  new ConcurrentKafkaListenerContainerFactory();
+
+    public ConcurrentKafkaListenerContainerFactory<Object,Object> concurrentKafkaListenerContainerFactory(){
+        ConcurrentKafkaListenerContainerFactory<Object,Object> factory =  new ConcurrentKafkaListenerContainerFactory<>();
         factory.setConsumerFactory(consumerFactory());
 
         return factory;
